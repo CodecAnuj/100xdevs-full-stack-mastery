a9bd910f4979a57a152a65b4f74e4334fc545be1
CodecAnuj <anujtiwari9111@gmail.com>
2025-05-06 14:11:02 +0530

    Created a webSocket-app

---
+++ 0-1/MyCode/Week 19/Week-19.1-WebSockets/my-react-app/src/App.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [socket, setSocket] = useState<null | WebSocket>(null)
+  const [latestmessage, setLatestMessage] = useState("")
+  const [message, setMessage] = useState("")
+
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:8080')
+    socket.onopen = () => {
+      console.log('Connected')
+      setSocket(socket)
+    }
+    socket.onmessage = (message) => {
+      console.log('Received message:', message.data)      
+      setLatestMessage(message.data)
+    }
+
+    return () => {
+      socket.close()
+    }
+
+  }, [])
+
+  if(!socket) {
+    return <div>
+      Connecting to socket server...
+    </div>
+  }
+
+  return (
+    <>
+        <input type="text" onChange={(e) => [
+          setMessage(e.target.value)
+        ]}/>
+        <button onClick={() => {
+          socket.send(message)
+        }}>Send</button>
+        {latestmessage}
+    </>
+  )
+}
+
+export default App
